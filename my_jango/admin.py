61585e8b7338d545d53febf1f895960eaf9cf6e8
masterShanto <mastershanto@gmail.com>
2026-03-09 02:58:03 +0600

    refactor: replace User model with AppUser; update admin and views; enhance Product model; add type hints and migrations

--- my_jango/admin.py
+++ my_jango/admin.py
@@ -1,40 +1,31 @@
-from typing import Sequence, Union, Callable, Any
 
 from django.contrib import admin
-from .models import User, Product
+
+from .models import AppUser, Product
 
 
-@admin.register(User)
-class UserAdmin(admin.ModelAdmin[User]):
-    list_display: Sequence[Union[str, Callable[[User], Any]]] = [
+@admin.register(AppUser)
+class AppUserAdmin(admin.ModelAdmin):  # type: ignore[misc]
+    list_display = [
         "name", "email", "phone", "created_at"
     ]
-    list_filter: Sequence[
-        Union[
-            str,
-            type[admin.ListFilter],
-            tuple[str, type[admin.ListFilter]],
-        ]
-    ] = ["created_at"]
-    search_fields: Sequence[str] = ["name", "email", "phone"]
-    readonly_fields: Sequence[Union[str, Callable[[User], Any]]] = [
+    list_filter = ["created_at"]
+    search_fields = ["name", "email", "phone"]
+    readonly_fields = [
         "created_at", "updated_at"
     ]
 
 
 @admin.register(Product)
-class ProductAdmin(admin.ModelAdmin[Product]):
-    list_display: Sequence[Union[str, Callable[[Product], Any]]] = [
-        "title", "price", "stock", "created_at",
+class ProductAdmin(admin.ModelAdmin):  # type: ignore[misc]
+    list_display = [
+        "title", "price", "stock", "created_at"
     ]
-    list_filter: Sequence[
-        Union[
-            str,
-            type[admin.ListFilter],
-            tuple[str, type[admin.ListFilter]],
-        ]
-    ] = ["created_at", "price"]
-    search_fields: Sequence[str] = ["title", "description"]
-    readonly_fields: Sequence[Union[str, Callable[[Product], Any]]] = [
-        "created_at",
+    list_filter = [
+        "created_at", "price"
     ]
+    search_fields = ["title", "description"]
+    readonly_fields = [
+        "created_at"
+    ]
+
